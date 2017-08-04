ba5a4c03b89edc43cb2b982e36717ae7e0afd904
Dom <dom.mcdonnell@gmail.com>
2017-08-04 21:11:51 +0100

    Can find match to give record

--- date_validator.rb
+++ date_validator.rb
@@ -1,1 +1,22 @@
-require 'models/product'
+require './models/product'
+
+class Validator
+
+products = Product.all
+
+  def display(records)
+    p 'ROW ID' + ' | ' + 'PRODUCT' + ' | ' + 'CUSTOMER' + ' | ' + 'MEASURE' + ' | ' + 'VALID FROM' + ' | ' + 'VALID TO'
+    records.each do |record|
+      p "#{record.id}" + ' | ' + "#{record.product}" + ' | ' + "#{record.customer}" + ' | ' + "#{record.measure}" + ' | ' + "#{record.value}" + ' | ' + "#{record.valid_from}" + ' | ' + "#{record.valid_to}"
+    end
+  end
+
+  def get_target(id)
+    Product.get(id)
+  end
+
+  def find_match(id)
+    target = get_target(id)
+    (Product.all(:product => target.product) & Product.all(:customer => target.customer) & Product.all(:measure => target.measure)).nil?
+  end
+end
--- spec/data_validator_spec.rb
+++ spec/data_validator_spec.rb
@@ -1,7 +1,21 @@
 require './models/product'
+require './date_validator'
 
 describe 'Data Validator: database' do
   it 'should be populated with sample data' do
     expect(Product.count).to eq 9
+  end
+end
+
+describe Validator do
+  let(:validator) { Validator.new }
+
+  it 'gets the first record from database' do
+    target = validator.get_target(1)
+    expect(target.id).to eq 1
+  end
+
+  it 'should return true is match is found' do
+    expect(validator.find_match(1)).to eq false
   end
 end
